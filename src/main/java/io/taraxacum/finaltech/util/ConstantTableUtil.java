e6349de0c843c715e6b69158e4d5ed641b99b66f
QYhB05 <140108356+QYhB05@users.noreply.github.com>
2025-03-21 22:37:02 +0800

    Update some constant data of final items

--- src/main/java/io/taraxacum/finaltech/util/ConstantTableUtil.java
+++ src/main/java/io/taraxacum/finaltech/util/ConstantTableUtil.java
@@ -13,9 +13,9 @@ public class ConstantTableUtil {
 
     public static final int ITEM_COPY_CARD_AMOUNT = ConfigUtil.getOrDefaultItemSetting(16777216, "COPY_CARD", "amount");
 
-    public static final int ITEM_SINGULARITY_AMOUNT = ConfigUtil.getOrDefaultItemSetting(256, "SINGULARITY", "amount") + Slimefun.getInstalledAddons().size() * 16;
+    public static final int ITEM_SINGULARITY_AMOUNT = ConfigUtil.getOrDefaultItemSetting(512, "SINGULARITY", "amount") + Slimefun.getInstalledAddons().size() * 32;
 
-    public static final int ITEM_SPIROCHETE_AMOUNT = ConfigUtil.getOrDefaultItemSetting(64, "SPIROCHETE", "amount") + (int) (Math.pow(Slimefun.getRegistry().getAllSlimefunItems().size(), 0.5) * 4.5);
+    public static final int ITEM_SPIROCHETE_AMOUNT = ConfigUtil.getOrDefaultItemSetting(256, "SPIROCHETE", "amount") + (int) (Math.pow(Slimefun.getRegistry().getAllSlimefunItems().size(), 0.5) * 16);
 
     public static final int ITEM_MAX_STACK = 64;
 
